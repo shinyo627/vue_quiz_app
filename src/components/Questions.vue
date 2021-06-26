1d5fcc70dc6975380d4688d1dc422d9b5a2d29f9
shinyo627 <shinyo627@gmail.com>
2021-06-26 12:25:08 +0200

    All the conditions rendering have been implemented for Questions, and Result components with logics

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,23 +1,50 @@
 <template>
   <div class="questions-ctr">
         <div class="progress">
-            <div class="bar"></div>
-            <div class="status">1 out of 3 questions answered</div>
+            <div class="bar" :style='{width: `${(questionsAnswered /  questions.length) * 100}%`}'></div>
+            <div class="status">{{questionsAnswered}} out of {{questions.length}} questions answered.</div>
         </div>
-        <div class="single-question">
-            <div class="question">Sample Question 1</div>
+
+        <transition-group name='fade'>
+        <div class="single-question" 
+        v-for='(question, questionIndex) in questions'
+        :key='question.q'
+        v-show='questionsAnswered === questionIndex'
+        >
+            <div class="question">{{question.q}}</div>
             <div class="answers">
-                <div class="answer">Sample Answer 1</div>
-                <div class="answer">Sample Answer 2</div>
-                <div class="answer">Sample Answer 3</div>
-                <div class="answer">Sample Answer 4</div>
+                <div class="answer"
+                v-for='answer in question.answers'
+                :key='answer.text'
+                @click.prevent='selectAnswer(answer.is_correct)'
+                >
+                {{answer.text}}
+                </div>
             </div>
         </div>
+        </transition-group>
+
     </div>
 </template>
 
 <script>
 export default {
-
+    props: {
+        questions: {
+            type: Array,
+            validator(value) {
+                return value.length > 0 
+            }
+        },
+        questionsAnswered: {
+            type: Number,
+        }
+    },
+    emits: ['question-answered'],
+    methods: {
+        selectAnswer(is_correct) {
+            this.$emit('question-answered', is_correct)
+        }
+    }
 }
 </script>
